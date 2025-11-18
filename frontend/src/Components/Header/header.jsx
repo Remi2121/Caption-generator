160a7dba65ff5849a6f30d5dcf55aa149a2c77db
Remi <remiluke12@gmail.com>
2025-11-18 11:18:46 +0530

    fist commit

---
+++ frontend/src/Components/Header/header.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './header.css'
+
+const header = () => {
+  return (
+    <div className='header-container'>
+      <ul className='navbar'>
+        <li>
+            Home
+        </li>
+        <li>
+            About US
+        </li>
+        <li>
+            Login
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default header
